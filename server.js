07fdbd72dad7149c33cf270efbbb48574d60566f
Alfredo Salazar <>
2019-11-08 07:34:06 -0600

    Added database and connected it to endpoints

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-let express = require('express');
-let morgan = require('morgan');
+let express = require( "express" );
+let morgan = require( "morgan" );
+let mongoose = require( "mongoose" );
+let bodyParser = require( "body-parser" );
+let { PetList } = require('./model');
 
 let app = express();
+let jsonParser = bodyParser.json();
+mongoose.Promise = global.Promise;
 
-app.use(express.static('public'));
-app.use( morgan( 'dev' ) );
+app.use( express.static( "public" ) );
+
+app.use( morgan( "dev" ) );
 
 let nameOfPets = [
 	{
@@ -25,24 +31,90 @@ let nameOfPets = [
 	}
 ];
 
-app.get( '/api/pets', (req, res, next) =>{
-	console.log( "Req query", req.query );
-	 return res.status(200).json( nameOfPets );
-
-	/*res.statusMessage = "Something went wrong.";
-	return res.status(400).json({
-		code : 400,
-		message : "Something went wrong!"
-	});*/
+app.get( "/api/pets", ( req, res, next ) => {
+	PetList.get()
+		.then( pets => {
+			return res.status( 200 ).json( pets );
+		})
+		.catch( error => {
+			res.statusMessage = "Something went wrong with the DB. Try again later.";
+			return res.status( 500 ).json({
+				status : 500,
+				message : "Something went wrong with the DB. Try again later."
+			})
+		});
 });
 
-app.get( '/api/pets/:id', (req, res, next) =>{
-	console.log( "Req param", req.params );
-	console.log( "Req query", req.query );
+app.post( "/api/postPet", jsonParser, ( req, res, next ) => {
+	let name = req.body.name;
+	let typeOfPet = req.body.typeOfPet;
+	let id = req.body.id;
 
-	return res.status(200).json( nameOfPets );
+	let newPet = {
+		name,
+		typeOfPet,
+		id
+	};
+
+	PetList.post(newPet)
+		.then( pet => {
+			return res.status( 201 ).json({
+				message : "Pet added to the list",
+				status : 201,
+				student : pet
+			});
+		})
+		.catch( error => {
+			res.statusMessage = "Something went wrong with the DB. Try again later.";
+			return res.status( 500 ).json({
+				status : 500,
+				message : "Something went wrong with the DB. Try again later."
+			});
+		});
 });
 
-app.listen( '8080', () => {
-	console.log( "App running on localhost:8080" );
-});
+let server;
+
+function runServer(port, databaseUrl){
+	return new Promise( (resolve, reject ) => {
+		mongoose.connect(databaseUrl, response => {
+			if ( response ){
+				return reject(response);
+			}
+			else{
+				server = app.listen(port, () => {
+					console.log( "App is running on port " + port );
+					resolve();
+				})
+				.on( 'error', err => {
+					mongoose.disconnect();
+					return reject(err);
+				})
+			}
+		});
+	});
+}
+
+function closeServer(){
+	return mongoose.disconnect()
+		.then(() => {
+			return new Promise((resolve, reject) => {
+				console.log('Closing the server');
+				server.close( err => {
+					if (err){
+						return reject(err);
+					}
+					else{
+						resolve();
+					}
+				});
+			});
+		});
+}
+
+runServer( 8080, "mongodb://localhost/petsDB" )
+	.catch( err => {
+		console.log( err );
+	});
+
+module.exports = { app, runServer, closeServer };
